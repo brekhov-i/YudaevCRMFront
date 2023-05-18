8ccf0f786f00a834ed74887206194030dce3adb1
brekhov-i <brekhov-i@yandex.ru>
2023-05-18 08:22:07 +0300

    add security route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import store from "@/store";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: () => import(/* webpackChunkName: "about" */ '../views/HomeView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/HomeView.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: "/login",
@@ -17,5 +19,15 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = store.getters["Auth/isAuth"]; // Проверка аутентификации пользователя
+  if (to.matched.some((route) => route.meta.requiresAuth) && !isAuthenticated) {
+    next("/login"); // Перенаправляем на страницу входа, если пользователь не аутентифицирован
+  } else {
+    next();
+    console.log(router)
+  }
+});
 
 export default router
